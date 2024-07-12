b4a60a51f83cb9617670f9851c839486b1868e4c
PhamCongHieuCNTT3 <phamhieu13062001@gmail.com>
2024-07-12 23:20:57 +0700

    lesson12

---
+++ PchK22CNT3Lesson12_2210900101/PchK22CNT3Lesson12_2210900101/Controllers/PchHomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PchK22CNT3Lesson12_2210900101.Controllers
+{
+    public class PchHomeController : Controller
+    {
+        public ActionResult PchIndex()
+        {
+            return View();
+        }
+
+        public ActionResult PchAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult PchContact()
+        {
+            ViewBag.msv = "2210900101";
+            ViewBag.fullname = "Phạm Công Hiếu";
+
+            return View();
+        }
+    }
+}
